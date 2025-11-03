11050e642769ad590fd30be6e9516771427aa2f7
save-on <sedmon033@gmail.com>
2025-11-03 17:53:18 -0600

    update readme progress

--- practice.cpp
+++ practice.cpp
@@ -1,32 +1,7 @@
 #include <iostream>
 
-int countdown(int n);
-int main() {
-  int startNumber{};
-  while (true) { 
-    std::cout << "Input a number greater than 0.. " << std::endl;
-    std::cin >> startNumber;
-    if (!startNumber && startNumber != 0) {
-      std::cout << "You must insert a number." << std::endl;
-      continue;
-    }
-    return countdown(startNumber);
-  }
-}
-
-int countdown(int n) {
-  if (n == 0) {
-    std::cout << "Blast off!!" << std::endl;
-    return 0;
-  }
-  
-  if (n < 0) {
-    std::cout << n << " Not amused.." << std::endl;
-    ++n;
-    return countdown(n);
-  }
-  
-  std::cout << n << "..." << std::endl;
-  --n;
-  return countdown(n);
+int main () {
+  int x{};
+  std::cout << sizeof(x) << '\n';
+  return 0;
 }
