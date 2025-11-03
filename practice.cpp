039aa4090e58896aad5d697ecd7e9a1877eae60f
save-on <sedmon033@gmail.com>
2025-11-03 12:30:12 -0600

    add recursive countdown function

--- practice.cpp
+++ practice.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
-#include <array>
 
-int add(int x);
-
+int countdown(int n);
 int main() {
-  int a{};
-  std::cin >> a;
-  std::cout << add(a) << std::endl;
-  return 0;
+  int startNumber{};
+  while (true) { 
+    std::cout << "Input a number greater than 0.. " << std::endl;
+    std::cin >> startNumber;
+    if (!startNumber && startNumber != 0) {
+      std::cout << "You must insert a number." << std::endl;
+      continue;
+    }
+    return countdown(startNumber);
+  }
 }
 
-int add(int element) {
-  if (!element) {
-    std::cout << "you need to input a number" << std::endl;
+int countdown(int n) {
+  if (n == 0) {
+    std::cout << "Blast off!!" << std::endl;
+    return 0;
   }
   
-  return element + 10;
+  if (n < 0) {
+    std::cout << n << " Not amused.." << std::endl;
+    ++n;
+    return countdown(n);
+  }
+  
+  std::cout << n << "..." << std::endl;
+  --n;
+  return countdown(n);
 }
