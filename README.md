ff7d5f0dd70f052be7d27693b8e3e56e765388ab
save-on <sedmon033@gmail.com>
2025-11-10 22:06:04 -0600

    update readme progress

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Current material - :hourglass:
 Finished material - :octocat:  
 
 ## C++ Video Material
-* [Mike Shah](https://www.youtube.com/watch?v=LGOgNqkRMs0&list=PLvv0ScY6vfd8j-tlhYVPYgiIyXduu6m-L) 25/231 video series - :hourglass
+* [Mike Shah](https://www.youtube.com/watch?v=LGOgNqkRMs0&list=PLvv0ScY6vfd8j-tlhYVPYgiIyXduu6m-L) 28/231 video series - :hourglass
 
 ## Course Material
 * [learncpp](https://www.learncpp.com)/2.3 - :hourglass:
--- practice.cpp
+++ practice.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
-
+#include <array>
+#include <string>
+#include <vector>
 int main() {
-
-    int numberOfStudents{};
-    std::cout << "How many students in our class?" << '\n';
-    std::cin >> numberOfStudents;
+    int x = 7;
+    int* px = &x;
+    int array[] = {1,3,5,7,9};
+    int* heapArray = new int[1000];
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(1);
+    v.push_back(1);
     
-    int* student_ids = new int[numberOfStudents];
-
-    for(int i = 0; i < numberOfStudents; i++) {
-        student_ids[i] = i;
-    }
-
-    delete[] student_ids; 
+    // outputs
+    std::cout << "x         : " << sizeof(x) << '\n'; 
+    std::cout << "px        : " << sizeof(px) << '\n'; 
+    std::cout << "array     : " << sizeof(array) << '\n'; 
+    std::cout << "heapArray : " << sizeof(heapArray) << '\n'; 
+    std::cout << "v         : " << sizeof(v) << '\n'; 
+    std::cout << "v's size  : " << v.size() << '\n';
     return 0;
 }
