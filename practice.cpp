206d2e0b27caeeace91022c54118025b13c89bf1
save-on <sedmon033@gmail.com>
2025-11-21 21:10:00 -0600

    update readme progress

--- practice.cpp
+++ practice.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
 
-int readNum() {
-    std::cout << "Enter a number: ";
-    int x{};
-    std::cin >> x;
-    return x;
-}
-
-void writeAns(int x) { std::cout << "The sum is: " << x << '\n'; }
-
-int main() {
-    int x{readNum()};
-    int y{readNum()};
-    writeAns(x + y);
-
-    return 0;
-}
