824a69be4850ec9e96f2b9c545c6e88d9f094c38
save-on <sedmon033@gmail.com>
2025-11-22 21:57:20 -0600

    update reamde progress

--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include <utility>
 
+int main() {
+
+    std::string &&myString = "copy";
+    std::string newString;
+
+    std::cout << "myString: " << myString << '\n';
+    std::cout << "newString: " << newString << '\n';
+
+    // newString = std::move(myString);
+    // newString = static_cast<std::string &&>(myString);
+    newString = myString;
+
+    std::cout << "myString: " << myString << '\n';
+    std::cout << "newString: " << newString << '\n';
+
+    return 0;
+}
