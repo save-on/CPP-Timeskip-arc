f4e6f4e2bec2bc039cd156c710eb409d387fafd4
save-on <sedmon033@gmail.com>
2025-12-18 18:07:02 -0600

    update readme progress

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <bitset>
+#include <cstdint>
 #include <iostream>
 
+std::bitset<4> rotl(std::bitset<4> bits) { return bits << 1 | bits >> 3; }
+
 int main() {
-    std::bitset<8> bin{5};
-    std::cout << "binary: " << bin << '\n';
-    bin.set(1);
-    std::cout << "altered: " << bin.count() << '\n';
+    std::bitset<4> bits1{0b0001};
+    std::cout << rotl(bits1) << '\n';
+
+    std::bitset<4> bits2{0b1001};
+    std::cout << rotl(bits2) << '\n';
+
     return 0;
 }
